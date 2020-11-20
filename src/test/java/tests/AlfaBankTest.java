cb956e24e24c49c0a189449f023d044f2f853dd8
Igor Pavlov <igortvk@ya.ru>
2020-11-20 15:49:12 +0300

    init commit

---
+++ src/test/java/tests/AlfaBankTest.java
@@ -0,0 +1,35 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class AlfaBankTest {
+
+    @BeforeAll
+    static void setup() {
+        Configuration.startMaximized = true; //размер задать как например 640-480???.
+    }
+
+    @Test
+    void testCountDeposit() {
+
+        open("https://alfabank.ru/make-money/");
+        $$(byText("Депозиты")).find(visible).parent().click();
+        $("[data-widget-name='Heading']").shouldHave(text("Вклады"));
+        $$(byText("Архивные счета и депозиты")).find(visible).click();
+        $("body").shouldHave(text("Архивные счета и депозиты"));
+        $$(byText("Депозиты")).find(visible).click();
+        $("#filter").findAll("[data-widget-name=CatalogCard]").shouldHaveSize(5);
+
+
+    }
+}
+
+
+
+
