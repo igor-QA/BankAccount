78adcf0ad3f52910b80ac09c2b1f218b31ccb097
Igor Pavlov <igortvk@ya.ru>
2020-11-23 00:55:51 +0300

    init commit

--- src/test/java/tests/AlfaBankTests.java
+++ src/test/java/tests/AlfaBankTests.java
@@ -8,29 +8,31 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class AlfaBankTest {
-    int count = 5;
+public class AlfaBankTests {
 
     @BeforeAll
     static void setup() {
-        Configuration.startMaximized = true; //размер задать как например 640-480???.
+        Configuration.startMaximized = true;
     }
 
     @Test
     void testCountDeposit() {
-
         open("https://alfabank.ru/make-money/");
         $$(byText("Депозиты")).find(visible).parent().click();
         $("[data-widget-name='Heading']").shouldHave(text("Вклады"));
         $$(byText("Архивные счета и депозиты")).find(visible).click();
         $("body").shouldHave(text("Архивные счета и депозиты"));
         $$(byText("Депозиты")).find(visible).click();
-        $("#filter").findAll("[data-widget-name=CatalogCard]").shouldHaveSize(count);
-
-
+        $("#filter").findAll("[data-widget-name=CatalogCard]").shouldHaveSize(5);
     }
+
+    @Test
+    void depositInsuranceTest() {
+        open("https://alfabank.ru");
+        $(byText("Вклады")).click();
+        $("[data-widget-name='Heading']").shouldHave(text("Накопительные продукты"));
+        $("[data-test-id='tabs-list']").sibling(0).click();
+        $("[data-widget-name='BlockV2']").sibling(2).shouldBe(visible);
+    }
+
 }
-
-
-
-
